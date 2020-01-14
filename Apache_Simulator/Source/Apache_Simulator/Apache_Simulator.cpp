96ec32b861325f3f03b7427dd99d1315faa8b928
Kieran Grist <kierangrist@gmail.com>
2020-01-14 15:31:04 +0000

    First Push

---
+++ Apache_Simulator/Source/Apache_Simulator/Apache_Simulator.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Apache_Simulator.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Apache_Simulator, "Apache_Simulator" );
